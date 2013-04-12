90ae12db8c90f0bc6dcb192e0f1950da89ac9620
Ondrej Sika <sika.ondrej@gmail.com>
2013-04-12 17:57:37 +0200

    git init; v.1.0

---
+++ README.md
@@ -0,0 +1,14 @@
+django-applabel
+===============
+
+Rename Django app label in admin
+
+Authors
+-------
+*  Ondrej Sika, <http://ondrejsika.com>, dev@ondrejsika.com
+
+Source
+------
+* Documentation: <http://ondrejsika.com/docs/django-applabel>
+* Python Package Index: <http://pypi.python.org/pypi/django-applabel>
+* GitHub: <https://github.com/sikaondrej/django-applabel>
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-applabel",
+    version = "1.0.0",
+    url = 'http://ondrejsika.com/docs/django-applabel',
+    download_url = 'https://github.com/sikaondrej/django-applabel',
+    license = 'GNU LGPL v.3',
+    description = "",
+    author = 'Ondrej Sika',
+    author_email = 'dev@ondrejsika.com',
+    py_modules = ["applabel"],
+    #packages = find_packages(),
+    install_requires = [],
+    include_package_data = True,
+    zip_safe = False,
+)
